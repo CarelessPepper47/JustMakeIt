84cbd157037150343e56fda1cd879ae4ef4a3774
MatiDev <155984203+CarelessPepper47@users.noreply.github.com>
2025-02-02 08:49:40 +0100

    player

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1024;
-canvas.height = 576;
+canvas.width = 800;
+canvas.height = 600;
 
-// const player = new Player{
-//     construct[    
-//         x:0,
-//         y:0];
-// };
+class Player {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0,
+        }
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, 100, 100)
+    }
+
+    update() {
+        this.draw()
+        this.position.y++
+    }
+}
+
+const player = new Player();
 
 let y = 100;
 function animate() {
     window.requestAnimationFrame(animate);
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, canvas.width, canvas.height);
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height)
 
-    c.fillStyle = 'red';
-    c.fillRect(200, y, 100, 100);
-    y++
+    player.update()
 }
 
 animate();
